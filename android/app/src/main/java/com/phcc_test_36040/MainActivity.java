3d2591dccd484f05d799291494728b6948a8d951
Crowdbotics <team@crowdbotics.com>
2022-07-05 06:24:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/phcc_test_36040/MainActivity.java
@@ -0,0 +1,15 @@
+package com.phcc_test_36040;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "phcc_test_36040";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "phcc_test_36040",
+  url: "https://phcc_test_36040.botics.co",
+  api: "https://phcc_test_36040.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
